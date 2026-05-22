5f4060cb36d66e0030fc623957b36bb69eed5791
Abdulbosid Tojimurodov <tojimurodovabdulbosid@gmail.com>
2026-05-22 22:33:17 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,31 @@
+import os
+
+# ====================== API ======================
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+
+# ====================== SESSION ======================
+
+STRING_SESSION = os.getenv("STRING_SESSION")
+
+SESSION_NAME = "userbot"
+
+# ====================== OWNER ======================
+
+OWNER_ID = int(os.getenv("OWNER_ID"))
+
+# ====================== PREFIX ======================
+
+PREFIX = "."
+
+# ====================== AUTO REPLY ======================
+
+AUTO_REPLY_MSG = (
+    "Salom 👋\n"
+    "Hozir bandman, keyinroq javob beraman."
+)
+
+# ====================== LOG CHANNEL ======================
+
+LOG_CHANNEL = "@userbotloglar"
